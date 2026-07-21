6f02ef8d2d92af3f99ddc01010ad75c01320b9f0
Supriya Kumari <supriyasingh2503@gmail.com>
2026-07-21 10:41:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
